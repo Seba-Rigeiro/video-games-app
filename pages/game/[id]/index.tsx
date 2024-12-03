98ca243c2bee36ba5a2f2bf6138023b76f6391e9
seba_rigeiro <srigeiro@ticmas.com>
2024-12-03 12:38:13 -0300

    responsive fix

--- pages/game/[id]/index.tsx
+++ pages/game/[id]/index.tsx
@@ -56,30 +56,42 @@ export default function GameDetailPage() {
   return (
     <Container maxWidth="md">
       <GradientContainer>
-        <Spacer size="12px" />
-        <Box display="flex" alignItems="center" gap={1.5}>
-          <IconButton style={{ color: "#3C1661" }} onClick={handleGoBack}>
-            <ArrowBackOutlinedIcon />
-          </IconButton>
-          <Typography
-            variant="subtitle1"
-            fontWeight={600}
-            style={{ color: "#3C1661" }}
-          >
-            Back
-          </Typography>
-        </Box>
-        <Spacer size="12px" />
+        <Spacer size="24px" />
         <Box
           display="flex"
+          flexDirection={isMobile ? "column" : "row"}
           alignItems={isMobile ? "none" : "center"}
-          flexDirection="column"
+          gap={1.5}
         >
-          <Search
-            onSearch={handleOnChangeSearchValue}
-            options={games}
-            isLoading={isSearchLoading}
-          />
+          <Box
+            display="flex"
+            alignItems="center"
+            gap={1.5}
+            pr={isMobile ? 0 : "80px"}
+          >
+            <IconButton style={{ color: "#3C1661" }} onClick={handleGoBack}>
+              <ArrowBackOutlinedIcon />
+            </IconButton>
+            <Typography
+              variant="subtitle1"
+              fontWeight={600}
+              style={{ color: "#3C1661" }}
+            >
+              Back
+            </Typography>
+          </Box>
+          <Box
+            display="flex"
+            alignItems={isMobile ? "none" : "center"}
+            flexDirection={isMobile ? "column" : "row"}
+            flexGrow={1}
+          >
+            <Search
+              onSearch={handleOnChangeSearchValue}
+              options={games}
+              isLoading={isSearchLoading}
+            />
+          </Box>
         </Box>
         <Spacer size="48px" />
         <GameDetail game={game} isMobile={isMobile} />
